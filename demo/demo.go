861a39301330099adaedb3e120c0433753958dd2
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-04-20 08:05:02 +1000

    start sample file

--- demo/demo.go
+++ demo/demo.go
@@ -13,19 +13,19 @@ func main() {
 	log.Println("Starting imagetemplate demo")
 	var builder img.Builder
 	var canvas img.Canvas
-	canvas, err := img.NewCanvas(100, 100)
+	canvas, err := img.NewCanvas(400, 400)
 	if err != nil {
 		log.Fatalf("Failed to create canvas: %v", err)
 	}
-	builder, err = img.NewBuilder(canvas, color.Gray{Y: 128})
+	builder, err = img.NewBuilder(canvas, color.Gray{Y: 180})
 	if err != nil {
 		log.Fatalf("Failed to create builder: %v", err)
 	}
-	err = canvas.Rectangle(image.Point{X: 10, Y: 40}, 15, 20, color.NRGBA{R: 255, G: 100, B: 0, A: 255})
+	err = canvas.Rectangle(image.Point{X: 110, Y: 40}, 60, 87, color.NRGBA{R: 255, G: 100, B: 0, A: 255})
 	if err != nil {
 		log.Fatalf("Failed to create rectangle: %v", err)
 	}
-	err = canvas.Circle(image.Point{X: 60, Y: 35}, 15, color.NRGBA{R: 0, G: 100, B: 255, A: 255})
+	err = canvas.Circle(image.Point{X: 301, Y: 253}, 57, color.NRGBA{R: 0, G: 100, B: 255, A: 255})
 	if err != nil {
 		log.Fatalf("Failed to create circle: %v", err)
 	}
