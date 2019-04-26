4fad7faa7ceb01b80631beeaa0e01b21296d13af
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-04-27 00:03:43 +1000

    add barcodes to canvas

--- canvas.go
+++ canvas.go
@@ -2,6 +2,17 @@ package imagetemplate
 
 import (
 	"errors"
+	"github.com/boombuler/barcode"
+	"github.com/boombuler/barcode/aztec"
+	"github.com/boombuler/barcode/codabar"
+	"github.com/boombuler/barcode/code128"
+	"github.com/boombuler/barcode/code39"
+	"github.com/boombuler/barcode/code93"
+	"github.com/boombuler/barcode/datamatrix"
+	"github.com/boombuler/barcode/ean"
+	"github.com/boombuler/barcode/pdf417"
+	"github.com/boombuler/barcode/qr"
+	"github.com/boombuler/barcode/twooffive"
 	"golang.org/x/image/font"
 	"image"
 	"image/color"
@@ -16,8 +27,9 @@ type Canvas interface {
 	GetHeight() int
 	Rectangle(topLeft image.Point, width, height int, colour color.Color) (Canvas, error)
 	Circle(centre image.Point, radius int, colour color.Color) (Canvas, error)
-	Text(start image.Point, typeFace font.Face, colour color.Color, fontSize, maxWidth, maxLines int) (Canvas, error)
+	Text(text string, start image.Point, typeFace font.Face, colour color.Color, fontSize, maxWidth, maxLines int) (Canvas, error)
 	SubImage(start image.Point, subImage image.Image) (Canvas, error)
+	Barcode(codeType BarcodeType, content []byte, extra BarcodeExtraData, start image.Point, width, height int) (Canvas, error)
 }
 
 // ImageCanvas uses golang's native Image package to implement the Canvas interface
@@ -88,9 +100,6 @@ func (canvas ImageCanvas) Rectangle(topLeft image.Point, width, height int, colo
 	return c, nil
 }
 
-// CircleAlignment dicates where precisely a circle is centred
-type CircleAlignment int
-
 // Circle draws a circle of a specific colour on the canvas
 func (canvas ImageCanvas) Circle(centre image.Point, radius int, colour color.Color) (Canvas, error) {
 	c := canvas
@@ -101,13 +110,156 @@ func (canvas ImageCanvas) Circle(centre image.Point, radius int, colour color.Co
 }
 
 // Text draws text on the canvas
-func (canvas ImageCanvas) Text(start image.Point, typeFace font.Face, colour color.Color, fontSize, maxWidth, maxLines int) (Canvas, error) {
-	return canvas, errors.New("Not implemented yet")
+func (canvas ImageCanvas) Text(text string, start image.Point, typeFace font.Face, colour color.Color, fontSize, maxWidth, maxLines int) (Canvas, error) {
+	c := canvas
+	drawer := &font.Drawer{
+		Dst:  c.Image,
+		Face: typeFace,
+		Src:  image.NewUniform(colour),
+	}
+	drawer.DrawString(text)
+	return c, nil
 }
 
 // SubImage draws another image on the canvas
 func (canvas ImageCanvas) SubImage(start image.Point, subImage image.Image) (Canvas, error) {
-	return canvas, errors.New("Not implemented yet")
+	c := canvas
+	draw.Draw(c.Image, subImage.Bounds(), subImage, start, draw.Over)
+	return c, nil
+}
+
+// BarcodeType wraps the barcode types into a single enum
+type BarcodeType string
+
+const (
+	// BarcodeTypeAztec           is an alias for an imported barcode type
+	BarcodeTypeAztec = barcode.TypeAztec
+	// BarcodeTypeCodabar         is an alias for an imported barcode type
+	BarcodeTypeCodabar = barcode.TypeCodabar
+	// BarcodeTypeCode128         is an alias for an imported barcode type
+	BarcodeTypeCode128 = barcode.TypeCode128
+	// BarcodeTypeCode39          is an alias for an imported barcode type
+	BarcodeTypeCode39 = barcode.TypeCode39
+	// BarcodeTypeCode93          is an alias for an imported barcode type
+	BarcodeTypeCode93 = barcode.TypeCode93
+	// BarcodeTypeDataMatrix      is an alias for an imported barcode type
+	BarcodeTypeDataMatrix = barcode.TypeDataMatrix
+	// BarcodeTypeEAN8            is an alias for an imported barcode type
+	BarcodeTypeEAN8 = barcode.TypeEAN8
+	// BarcodeTypeEAN13           is an alias for an imported barcode type
+	BarcodeTypeEAN13 = barcode.TypeEAN13
+	// BarcodeTypePDF             is an alias for an imported barcode type
+	BarcodeTypePDF = barcode.TypePDF
+	// BarcodeTypeQR              is an alias for an imported barcode type
+	BarcodeTypeQR = barcode.TypeQR
+	// BarcodeType2of5            is an alias for an imported barcode type
+	BarcodeType2of5 = barcode.Type2of5
+	// BarcodeType2of5Interleaved is an alias for an imported barcode type
+	BarcodeType2of5Interleaved = barcode.Type2of5Interleaved
+)
+
+// BarcodeExtraData contains additional data required for some barcode formats, leave any fields not named for the type in use alone
+type BarcodeExtraData struct {
+	// AztecMinECCPercent       is required for aztec barcodes
+	AztecMinECCPercent int
+	// AztecUserSpecifiedLayers is required for aztec barcodes
+	AztecUserSpecifiedLayers int
+	// Code39IncludeChecksum    is required for code39 barcodes
+	Code39IncludeChecksum bool
+	// Code39FullAsciiMode      is required for code39 barcodes
+	Code39FullAsciiMode bool
+	// Code93IncludeChecksum    is required for code93 barcodes
+	Code93IncludeChecksum bool
+	// Code93FullAsciiMode      is required for code93 barcodes
+	Code93FullAsciiMode bool
+	// PDFSecurityLevel         is required for pdf417 barcodes
+	PDFSecurityLevel byte
+	// QRLevel                  is required for qr barcodes
+	QRLevel qr.ErrorCorrectionLevel
+	// QRMode                   is required for qr barcodes
+	QRMode qr.Encoding
+}
+
+// Barcode draws a barcode on the canvas
+func (canvas ImageCanvas) Barcode(codeType BarcodeType, content []byte, extra BarcodeExtraData, start image.Point, width, height int) (Canvas, error) {
+	c := canvas
+	var encodedBarcode barcode.Barcode
+	var err error
+	switch codeType {
+	case BarcodeTypeAztec:
+		encodedBarcode, err = aztec.Encode(content, extra.AztecMinECCPercent, extra.AztecUserSpecifiedLayers)
+		if err != nil {
+			return canvas, err
+		}
+	case BarcodeTypeCodabar:
+		encodedBarcode, err = codabar.Encode(string(content))
+		if err != nil {
+			return canvas, err
+		}
+	case BarcodeTypeCode128:
+		encodedBarcode, err = code128.Encode(string(content))
+		if err != nil {
+			return canvas, err
+		}
+	case BarcodeTypeCode39:
+		encodedBarcode, err = code39.Encode(string(content), extra.Code39IncludeChecksum, extra.Code39FullAsciiMode)
+		if err != nil {
+			return canvas, err
+		}
+	case BarcodeTypeCode93:
+		encodedBarcode, err = code93.Encode(string(content), extra.Code93IncludeChecksum, extra.Code93FullAsciiMode)
+		if err != nil {
+			return canvas, err
+		}
+	case BarcodeTypeDataMatrix:
+		encodedBarcode, err = datamatrix.Encode(string(content))
+		if err != nil {
+			return canvas, err
+		}
+	case BarcodeTypeEAN8:
+		if len(content) != 8 {
+			return canvas, errors.New("EAN8 Barcode requires 8 characters")
+		}
+		encodedBarcode, err = ean.Encode(string(content))
+		if err != nil {
+			return canvas, err
+		}
+	case BarcodeTypeEAN13:
+		if len(content) != 13 {
+			return canvas, errors.New("EAN13 Barcode requires 13 characters")
+		}
+		encodedBarcode, err = ean.Encode(string(content))
+		if err != nil {
+			return canvas, err
+		}
+	case BarcodeTypePDF:
+		encodedBarcode, err = pdf417.Encode(string(content), extra.PDFSecurityLevel)
+		if err != nil {
+			return canvas, err
+		}
+	case BarcodeTypeQR:
+		encodedBarcode, err = qr.Encode(string(content), extra.QRLevel, extra.QRMode)
+		if err != nil {
+			return canvas, err
+		}
+	case BarcodeType2of5:
+		encodedBarcode, err = twooffive.Encode(string(content), false)
+		if err != nil {
+			return canvas, err
+		}
+	case BarcodeType2of5Interleaved:
+		encodedBarcode, err = twooffive.Encode(string(content), true)
+		if err != nil {
+			return canvas, err
+		}
+	}
+	encodedBarcode, err = barcode.Scale(encodedBarcode, width, height)
+	if err != nil {
+		return canvas, err
+	}
+	//TODO: add support for colouring barcode and square, including transparency, using masks
+	draw.Draw(c.Image, image.Rect(start.X, start.Y, start.X+width, start.Y+height), encodedBarcode, image.ZP, draw.Over)
+	return c, nil
 }
 
 // Steal the circle example code from https://blog.golang.org/go-imagedraw-package
