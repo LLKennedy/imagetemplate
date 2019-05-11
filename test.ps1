5efcc888c4236fec6d40d9a027c028ca9adcafa9
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-05-11 22:11:17 +1000

    only render html if tests pass

--- test.ps1
+++ test.ps1
@@ -1,4 +1,6 @@
 go test ./... -cover -coverprofile="coverage.out"; 
-go tool cover -html="coverage.out";
-go tool cover -func="coverage.out";
+if ($LastExitCode -eq 0) {
+	go tool cover -html="coverage.out";
+	go tool cover -func="coverage.out";
+}
 Remove-Item coverage.out;
