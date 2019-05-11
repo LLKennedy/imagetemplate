06457f62fe18c971a22a51458a2825b91d3fe4b0
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-05-11 21:25:31 +1000

    update test function to match new package structure

--- test.ps1
+++ test.ps1
@@ -1,3 +1,4 @@
-go test . -cover -coverprofile="coverage.out"; 
+go test ./... -cover -coverprofile="coverage.out"; 
 go tool cover -html="coverage.out";
+go tool cover -func="coverage.out";
 Remove-Item coverage.out;
