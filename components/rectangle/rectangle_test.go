25f3010faaf03a927f644df346067fdff9ae2fe6
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-05-20 09:21:45 +1000

    Basic coverage of rectangle component

--- components/rectangle/rectangle_test.go
+++ components/rectangle/rectangle_test.go
@@ -1,1 +1,248 @@
 package rectangle
+
+import (
+	"errors"
+	"image"
+	"image/color"
+	"testing"
+
+	"github.com/LLKennedy/imagetemplate/render"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRectangleWrite(t *testing.T) {
+	t.Run("not all props set", func(t *testing.T) {
+		canvas := render.MockCanvas{}
+		c := Component{NamedPropertiesMap: map[string][]string{"not set": []string{"something"}}}
+		modifiedCanvas, err := c.Write(canvas)
+		assert.Equal(t, canvas, modifiedCanvas)
+		assert.EqualError(t, err, "cannot draw rectangle, not all named properties are set: map[not set:[something]]")
+	})
+	t.Run("rectangle error", func(t *testing.T) {
+		canvas := render.MockCanvas{FixedRectangleError: errors.New("some error")}
+		c := Component{}
+		modifiedCanvas, err := c.Write(canvas)
+		assert.Equal(t, canvas, modifiedCanvas)
+		assert.EqualError(t, err, "some error")
+	})
+	t.Run("passing", func(t *testing.T) {
+		canvas := render.MockCanvas{FixedRectangleError: nil}
+		c := Component{}
+		modifiedCanvas, err := c.Write(canvas)
+		assert.Equal(t, canvas, modifiedCanvas)
+		assert.NoError(t, err)
+	})
+}
+
+func TestRectangleSetNamedProperties(t *testing.T) {
+	type testSet struct {
+		name  string
+		start Component
+		input render.NamedProperties
+		res   Component
+		err   string
+	}
+	tests := []testSet{
+		testSet{
+			name:  "no props",
+			start: Component{},
+			input: render.NamedProperties{},
+			res:   Component{},
+			err:   "",
+		},
+		testSet{
+			name: "RGBA invalid",
+			start: Component{
+				NamedPropertiesMap: map[string][]string{
+					"aProp": []string{"R"},
+				},
+			},
+			input: render.NamedProperties{
+				"aProp": "not a number",
+			},
+			res: Component{
+				NamedPropertiesMap: map[string][]string{
+					"aProp": []string{"R"},
+				},
+			},
+			err: "error converting not a number to uint8",
+		},
+		testSet{
+			name: "RGBA valid",
+			start: Component{
+				NamedPropertiesMap: map[string][]string{
+					"aProp": []string{"R", "G", "B", "A"},
+				},
+			},
+			input: render.NamedProperties{
+				"aProp": uint8(1),
+			},
+			res: Component{
+				NamedPropertiesMap: map[string][]string{},
+				Colour:             color.NRGBA{R: uint8(1), G: uint8(1), B: uint8(1), A: uint8(1)},
+			},
+			err: "",
+		},
+		testSet{
+			name: "non-RGBA invalid type",
+			start: Component{
+				NamedPropertiesMap: map[string][]string{
+					"aProp": []string{"not a prop"},
+				},
+			},
+			input: render.NamedProperties{
+				"aProp": "not a number",
+			},
+			res: Component{
+				NamedPropertiesMap: map[string][]string{
+					"aProp": []string{"not a prop"},
+				},
+			},
+			err: "error converting not a number to int",
+		},
+		testSet{
+			name: "non-RGBA invalid name",
+			start: Component{
+				NamedPropertiesMap: map[string][]string{
+					"aProp": []string{"not a prop"},
+				},
+			},
+			input: render.NamedProperties{
+				"aProp": 12,
+			},
+			res: Component{
+				NamedPropertiesMap: map[string][]string{
+					"aProp": []string{"not a prop"},
+				},
+			},
+			err: "invalid component property in named property map: not a prop",
+		},
+		testSet{
+			name: "topLeftX",
+			start: Component{
+				NamedPropertiesMap: map[string][]string{
+					"aProp": []string{"topLeftX"},
+				},
+			},
+			input: render.NamedProperties{
+				"aProp": 15,
+			},
+			res: Component{
+				NamedPropertiesMap: map[string][]string{},
+				TopLeft:            image.Pt(15, 0),
+			},
+			err: "",
+		},
+		testSet{
+			name: "topLeftY",
+			start: Component{
+				NamedPropertiesMap: map[string][]string{
+					"aProp": []string{"topLeftY"},
+				},
+			},
+			input: render.NamedProperties{
+				"aProp": 15,
+			},
+			res: Component{
+				NamedPropertiesMap: map[string][]string{},
+				TopLeft:            image.Pt(0, 15),
+			},
+			err: "",
+		},
+		testSet{
+			name: "width",
+			start: Component{
+				NamedPropertiesMap: map[string][]string{
+					"aProp": []string{"width"},
+				},
+			},
+			input: render.NamedProperties{
+				"aProp": 15,
+			},
+			res: Component{
+				NamedPropertiesMap: map[string][]string{},
+				Width:              15,
+			},
+			err: "",
+		},
+		testSet{
+			name: "full prop set, multiple sources, unused props",
+			start: Component{
+				NamedPropertiesMap: map[string][]string{
+					"col1": []string{"R", "G", "A"},
+					"left": []string{"topLeftX"},
+					"wide": []string{"width", "topLeftY"},
+					"col3": []string{"B"},
+					"what": []string{"R", "G", "B", "A", "topLeftX"},
+				},
+			},
+			input: render.NamedProperties{
+				"col1":             uint8(15),
+				"col2":             uint8(6),
+				"up":               50,
+				"some other thing": "doesn't matter",
+				"col3":             uint8(150),
+				"wide":             80,
+				"left":             3,
+			},
+			res: Component{
+				NamedPropertiesMap: map[string][]string{"what": []string{"R", "G", "B", "A", "topLeftX"}},
+				TopLeft:            image.Pt(3, 80),
+				Width:              80,
+				Colour:             color.NRGBA{R: uint8(15), G: uint8(15), B: uint8(150), A: uint8(15)},
+			},
+			err: "",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res, err := test.start.SetNamedProperties(test.input)
+			assert.Equal(t, test.res, res)
+			if test.err == "" {
+				assert.NoError(t, err)
+			} else {
+				assert.EqualError(t, err, test.err)
+			}
+		})
+	}
+}
+
+func TestRectangleGetJSONFormat(t *testing.T) {
+	c := Component{}
+	expectedFormat := &rectangleFormat{}
+	format := c.GetJSONFormat()
+	assert.Equal(t, expectedFormat, format)
+}
+
+func TestRectangleVerifyAndTestRectangleJSONData(t *testing.T) {
+	type testSet struct {
+		name  string
+		start Component
+		input interface{}
+		res   Component
+		props render.NamedProperties
+		err   string
+	}
+	tests := []testSet{
+		testSet{
+			name:  "incorrect format data",
+			start: Component{},
+			input: "hello",
+			res:   Component{},
+			props: render.NamedProperties{},
+			err:   "failed to convert returned data to component properties",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res, props, err := test.start.VerifyAndSetJSONData(test.input)
+			assert.Equal(t, test.res, res)
+			assert.Equal(t, test.props, props)
+			if test.err == "" {
+				assert.NoError(t, err)
+			} else {
+				assert.EqualError(t, err, test.err)
+			}
+		})
+	}
+}
