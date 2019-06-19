7ffc33556594071438f6a056c83ce089231cdee3
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-06-19 17:28:46 +0930

    Cover filesystem methods

--- internal/filesystem/mockfs_test.go
+++ internal/filesystem/mockfs_test.go
@@ -4,9 +4,11 @@ import (
 	"errors"
 	"io"
 	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/tools/godoc/vfs"
 )
 
 func TestMockFS(t *testing.T) {
@@ -45,6 +47,32 @@ func TestMockFS(t *testing.T) {
 		assert.NoError(t, res.Close())
 	})
 	t.Run("lstat", func(t *testing.T) {
-		m.On("Lstat", "a").Return()
+		m.On("Lstat", "a").Return(NewMockFile(nil), nil)
+		stats, err := m.Lstat("a")
+		assert.Equal(t, NewMockFile(nil), stats)
+		assert.NoError(t, err)
 	})
+	t.Run("stat", func(t *testing.T) {
+		m.On("Stat", "b").Return(NewMockFile([]byte{}), errors.New("some error"))
+		stats, err := m.Stat("b")
+		assert.Equal(t, NewMockFile([]byte{}), stats)
+		assert.EqualError(t, err, "some error")
+	})
+	t.Run("readdir", func(t *testing.T) {
+		m.On("ReadDir", "c").Return([]os.FileInfo{NewMockFile(nil)}, nil)
+		files, err := m.ReadDir("c")
+		assert.NoError(t, err)
+		assert.Equal(t, []os.FileInfo{NewMockFile(nil)}, files)
+	})
+	t.Run("roottype", func(t *testing.T) {
+		m.On("RootType", "d").Return(vfs.RootTypeGoPath)
+		rtype := m.RootType("d")
+		assert.Equal(t, vfs.RootTypeGoPath, rtype)
+	})
+	t.Run("string", func(t *testing.T) {
+		m.On("String").Return("hello")
+		str := m.String()
+		assert.Equal(t, "hello", str)
+	})
+	m.AssertExpectations(t)
 }
