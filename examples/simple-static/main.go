d32dcada07084c6e523608d17f44e5ee1790ecf8
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-06-20 09:45:35 +0930

    Update simple static example to use new interface

--- examples/simple-static/main.go
+++ examples/simple-static/main.go
@@ -9,12 +9,12 @@ import (
 )
 
 func main() {
-	props, callback, err := imagetemplate.LoadTemplate("template.json")
+	loader, props, err := imagetemplate.New(nil).Load().FromFile("template.json")
 	if err != nil {
 		fmt.Printf("failed to load file: %v\n", err)
 		os.Exit(1)
 	}
-	data, err := callback(props)
+	data, err := loader.Write().ToBMP(props)
 	err = ioutil.WriteFile("simple-static.bmp", data, os.ModeExclusive)
 	if err != nil {
 		fmt.Printf("failed to write file: %v\n", err)
