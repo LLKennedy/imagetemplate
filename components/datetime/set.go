79e1c44c617e9ed88d34d2e33bfd24d3d7f27d9b
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-06-27 20:35:51 +0930

    Split delegated set properties into individual functions

--- components/datetime/set.go
+++ components/datetime/set.go
@@ -3,7 +3,6 @@ package datetime
 import (
 	"fmt"
 	"io/ioutil"
-	"strings"
 	"time"
 
 	"github.com/golang/freetype/truetype"
@@ -13,137 +12,178 @@ import (
 func (component *Component) delegatedSetProperties(name string, value interface{}) (err error) {
 	switch name {
 	case "time":
-		stringVal, isStrings := value.([]string)
-		timePointer, isTimePointer := value.(*time.Time)
-		timeVal, isTime := value.(time.Time)
-		if (!isStrings && !isTimePointer && !isTime) || (isStrings && len(stringVal) != 2) {
-			return fmt.Errorf("error converting %v to []string, *time.Time or time.Time", value)
-		}
-		if isTime {
-			component.Time = &timeVal
-		}
-		if isTimePointer {
-			component.Time = timePointer
-		}
-		if isStrings {
-			timeVal, err := time.Parse(stringVal[0], stringVal[1])
-			if err != nil {
-				return fmt.Errorf("cannot convert time string %v to time format %v", stringVal[1], stringVal[0])
-			}
-			component.Time = &timeVal
-		}
-		return nil
+		err = component.setTime(value)
 	case "timeFormat":
-		stringVal, ok := value.(string)
-		if !ok {
-			return fmt.Errorf("error converting %v to string", value)
-		}
-		component.TimeFormat = stringVal
-		return nil
+		err = component.setTimeFormat(value)
 	case "fontName":
-		stringVal, ok := value.(string)
-		if !ok {
-			return fmt.Errorf("error converting %v to string", value)
-		}
-		rawFont, err := component.getFontPool().GetFont(stringVal)
-		if err != nil {
-			return err
-		}
-		component.Font = rawFont
-		return nil
+		err = component.setFontName(value)
 	case "fontFile":
-		stringVal, ok := value.(string)
-		if !ok {
-			return fmt.Errorf("error converting %v to string", value)
-		}
-		if component.fs == nil {
-			component.fs = vfs.OS(".")
-		}
-		fontReader, err := component.fs.Open(stringVal)
-		if err != nil {
-			return err
-		}
-		defer fontReader.Close()
-		fontData, err := ioutil.ReadAll(fontReader)
-		if err != nil {
-			return err
-		}
-		rawFont, err := truetype.Parse(fontData)
-		if err != nil {
-			return err
-		}
-		component.Font = rawFont
-		return nil
+		err = component.setFontFile(value)
 	case "fontURL":
-		return fmt.Errorf("fontURL not implemented")
+		err = component.setFontURL(value)
 	case "size":
-		float64Val, ok := value.(float64)
-		if !ok {
-			return fmt.Errorf("error converting %v to float64", value)
-		}
-		component.Size = float64Val
-		return nil
+		err = component.setSize(value)
 	case "alignment":
-		alignmentVal, isAlignment := value.(Alignment)
-		stringVal, isString := value.(string)
-		if !isAlignment && !isString {
-			return fmt.Errorf("could not convert %v to datetime alignment or string", value)
-		}
-		if isAlignment {
-			component.Alignment = alignmentVal
-			return nil
-		}
-		switch stringVal {
-		case "left":
-			component.Alignment = AlignmentLeft
-			return nil
-		case "right":
-			component.Alignment = AlignmentRight
-			return nil
-		case "centre":
-			component.Alignment = AlignmentCentre
-			return nil
-		default:
-			component.Alignment = AlignmentLeft
-			return nil
-		}
+		err = component.setAlignment(value)
+	case "R", "G", "B", "A":
+		err = component.setColour(name, value)
+	case "startX", "startY":
+		err = component.setStart(name, value)
+	case "maxWidth":
+		err = component.setMaxWidth(value)
+	default:
+		err = fmt.Errorf("invalid component property in named property map: %v", name)
 	}
-	if strings.Contains("RGBA", name) && len(name) == 1 {
-		//Process colours
-		colourVal, ok := value.(uint8)
+	return
+}
+
+func (component *Component) setTime(value interface{}) error {
+	stringVal, isStrings := value.([]string)
+	timePointer, isTimePointer := value.(*time.Time)
+	timeVal, isTime := value.(time.Time)
+	if (!isStrings && !isTimePointer && !isTime) || (isStrings && len(stringVal) != 2) {
+		return fmt.Errorf("error converting %v to []string, *time.Time or time.Time", value)
+	}
+	if isTime {
+		component.Time = &timeVal
+	}
+	if isTimePointer {
+		component.Time = timePointer
+	}
+	if isStrings {
+		timeVal, err := time.Parse(stringVal[0], stringVal[1])
+		if err != nil {
+			return fmt.Errorf("cannot convert time string %v to time format %v", stringVal[1], stringVal[0])
+		}
+		component.Time = &timeVal
+	}
+	return nil
+}
+
+func (component *Component) setTimeFormat(value interface{}) error {
+	stringVal, ok := value.(string)
+	if !ok {
+		return fmt.Errorf("error converting %v to string", value)
+	}
+	component.TimeFormat = stringVal
+	return nil
+}
+
+func (component *Component) setFontName(value interface{}) error {
+	stringVal, ok := value.(string)
+	if !ok {
+		return fmt.Errorf("error converting %v to string", value)
+	}
+	rawFont, err := component.getFontPool().GetFont(stringVal)
+	if err != nil {
+		return err
+	}
+	component.Font = rawFont
+	return nil
+}
+
+func (component *Component) setFontFile(value interface{}) error {
+	stringVal, ok := value.(string)
+	if !ok {
+		return fmt.Errorf("error converting %v to string", value)
+	}
+	if component.fs == nil {
+		component.fs = vfs.OS(".")
+	}
+	fontReader, err := component.fs.Open(stringVal)
+	if err != nil {
+		return err
+	}
+	defer fontReader.Close()
+	fontData, err := ioutil.ReadAll(fontReader)
+	if err != nil {
+		return err
+	}
+	rawFont, err := truetype.Parse(fontData)
+	if err != nil {
+		return err
+	}
+	component.Font = rawFont
+	return nil
+}
+
+func (component *Component) setFontURL(value interface{}) error {
+	return fmt.Errorf("fontURL not implemented")
+}
+
+func (component *Component) setSize(value interface{}) error {
+	float64Val, ok := value.(float64)
+	if !ok {
+		return fmt.Errorf("error converting %v to float64", value)
+	}
+	component.Size = float64Val
+	return nil
+}
+
+func (component *Component) setAlignment(value interface{}) error {
+	alignmentVal, isAlignment := value.(Alignment)
+	stringVal, isString := value.(string)
+	if !isAlignment && !isString {
+		return fmt.Errorf("could not convert %v to datetime alignment or string", value)
+	}
+	if isAlignment {
+		component.Alignment = alignmentVal
+		return nil
+	}
+	switch stringVal {
+	case "left":
+		component.Alignment = AlignmentLeft
+	case "right":
+		component.Alignment = AlignmentRight
+	case "centre":
+		component.Alignment = AlignmentCentre
+	default:
+		component.Alignment = AlignmentLeft
+	}
+	return nil
+}
+
+func (component *Component) setColour(name string, value interface{}) error {
+	//Process colours
+	colourVal, ok := value.(uint8)
+	if !ok {
+		return fmt.Errorf("error converting %v to uint8", value)
+	}
+	switch name {
+	case "R":
+		component.Colour.R = colourVal
+	case "G":
+		component.Colour.G = colourVal
+	case "B":
+		component.Colour.B = colourVal
+	default:
+		component.Colour.A = colourVal
+	}
+	return nil
+}
+
+func (component *Component) setStart(name string, value interface{}) error {
+	if name == "startX" {
+		numberVal, ok := value.(int)
 		if !ok {
-			return fmt.Errorf("error converting %v to uint8", value)
+			return fmt.Errorf("error converting %v to int", value)
 		}
-		switch name {
-		case "R":
-			component.Colour.R = colourVal
-			return nil
-		case "G":
-			component.Colour.G = colourVal
-			return nil
-		case "B":
-			component.Colour.B = colourVal
-			return nil
-		case "A":
-			component.Colour.A = colourVal
-			return nil
-		}
+		component.Start.X = numberVal
+		return nil
 	}
 	numberVal, ok := value.(int)
 	if !ok {
 		return fmt.Errorf("error converting %v to int", value)
 	}
-	switch name {
-	case "startX":
-		component.Start.X = numberVal
-		return nil
-	case "startY":
-		component.Start.Y = numberVal
-		return nil
-	case "maxWidth":
-		component.MaxWidth = numberVal
-		return nil
-	default:
-		return fmt.Errorf("invalid component property in named property map: %v", name)
+	component.Start.Y = numberVal
+	return nil
+}
+
+func (component *Component) setMaxWidth(value interface{}) error {
+	numberVal, ok := value.(int)
+	if !ok {
+		return fmt.Errorf("error converting %v to int", value)
 	}
+	component.MaxWidth = numberVal
+	return nil
 }
